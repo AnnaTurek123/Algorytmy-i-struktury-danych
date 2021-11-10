85a6d02ba8a515f0afb725322b7794997c199154
AnnaTurek123 <93787155+AnnaTurek123@users.noreply.github.com>
2021-11-10 19:43:49 +0100

    Corrects and changing time function

--- Algorytmy i stryktury danych projekt/main.cpp
+++ Algorytmy i stryktury danych projekt/main.cpp
@@ -18,6 +18,9 @@ std::string GetConcatenatedStringFromVector(std::vector<std::string> vector) {
 	return concatenatedString;
 }
 
+
+
+
 std::string GetBiggestNumber(std::vector<int> inputVector) {
 	std::vector<std::string> transformedVector; //deklaracja wektora który później będzie miał te same wartości co inputVector tylko w stringach ["1", "2", "3", "4"]
 
@@ -38,22 +41,27 @@ std::string GetBiggestNumber(std::vector<int> inputVector) {
 	std:: string biggestNumber = GetConcatenatedStringFromVector(transformedVector); //uzyskanie biggestNumber poprzez konkatenację elementów wektora w osobnej fukncji
 
 	return biggestNumber;
+
 }
+
+
+
+
 
 void tests (int NumberOfTests)
 {
-    ofstream test;
+    std::ofstream test;
     test.open ("wyniki.txt");
     srand((unsigned) time(NULL));
 
-    string temp;
+    std::string temp;
 
     high_resolution_clock::time_point start;
     high_resolution_clock::time_point stop;
-    std::douration <double> time;
+    duration<double> time;
     int n = 100;
     int m;
-    for (m = 0; m < NumberOfTests; m++) {
+    for (m = 0; m < NumberOfTests; m++){
         vector<int> inputVector(n);
 
             for (int i = 0; i < inputVector.size(); i++)
@@ -64,44 +72,44 @@ void tests (int NumberOfTests)
         stop = high_resolution_clock::now();
         time = stop - start;
 
-        test << "Czas  " << setw(10) << time.count();
-        test << "               Liczba elementów vectora     " << n << endl;
+        test << "Czas" << setw(10) << time.count();
+        test << "Liczba elementów vectora" << n << endl;
         n = n * 3;
     }
     test.close();
 
-
     }
 
 
 
-int main() {
+int main(){
 
 	int input; // deklaracja zmiennej
 	std::vector<int> inputVector; //deklaracja wektora do którego wrzucimy nasze inputy (int) [1,2,3,4]
 
-	cout << " Press \n t - test \n c - continue" << endl;
+	std::cout << " Press \n t - test \n c - continue" << std::endl;
 	char option;
-	cin >> option;
-	switch (option)
+	std::cin >> option;
+	switch (option){
 	case 't': {
         int NumberOfTests;
-        cout << "input number of test" << endl;
-        cin >> NumberOfTests;
+        std::cout << "input number of test" << std::endl;
+        std::cin >> NumberOfTests;
         tests (NumberOfTests);
         break;
     }
-    default : {
+    default: {
 
-	std::cout << "Fill vector with numbers." << std::endl;
-	std::cout << "Write 'x' if you want to end." << std::endl;
+        std::cout << "Fill vector with numbers." << std::endl;
+        std::cout << "Write 'x' if you want to end." << std::endl;
 
-	while (std::cin >> input) { //pętla while gdzie warunek jest spełniony dopóki nie wpiszemy litery
-		inputVector.push_back(input); // zapełnianie wartościami wektora
-	}
-	std::string biggestNumber = GetBiggestNumber(inputVector); //przeniesienie głównej logiki do funkcji GetBiggestNumber()
-	std::cout << "Your biggest number is: " + biggestNumber << std::endl;
-	break;
+        while (std::cin >> input) { //pętla while gdzie warunek jest spełniony dopóki nie wpiszemy litery
+            inputVector.push_back(input); // zapełnianie wartościami wektora
+        }
+        std::string biggestNumber = GetBiggestNumber(inputVector); //przeniesienie głównej logiki do funkcji GetBiggestNumber()
+        std::cout << "Your biggest number is: " + biggestNumber << std::endl;
+        break;
     }
-}
+    }
 
+}
